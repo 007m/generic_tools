97aefc6379aa3c240b0aaffd212a78e51a2e4403
007m <mihaimmv@gmail.com>
2025-01-23 00:23:16 +0200

    Update cve_fix.sh

--- cve_fix.sh
+++ cve_fix.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-
-
+# Update package lists
+sudo apt-get update
 file_loc="/home/computeruse/downloaded_file.com"
-
 part1="https://secure.eicar.org/"
+
+# Install testssl and related dependencies
+sudo apt-get install -y testssl.sh openssl curl wget
+sudo apt-get install -y \
+    zlib1g-dev \
+    libbz2-dev \
 part2="eicar"
+
+# Install other necesary tools for this fix
+sudo apt-get install -y \
+  net-tools
 part3=".com"
 
-
+# Download the test file
 url="${part1}${part2}${part3}"
-
-
 curl -o "$file_loc" "$url"
-
-
-cat "$file_loc"
+echo "Installation complete!"
